83d957b7d224a05ada6c5e3968ce00c92065a087
Mrfamzy1 <111088104+Mrfamzy1@users.noreply.github.com>
2022-11-17 05:19:57 +0100

    update

--- getLine.c
+++ getLine.c
@@ -45,7 +45,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 }
 
 /**
- * get_input - gets a line minus the newline
+ * get_input - gets a line minus the newline.
  * @info: parameter struct
  *
  * Return: bytes read
